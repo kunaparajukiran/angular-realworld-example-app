16516ed323fcb321803167a80a089e39c6c32c43
kunaparajukiran <kirankunapa@gmail.com>
2022-08-29 12:24:34 +0000

    updated file

--- functions/time.js
+++ functions/time.js
@@ -1,17 +1,17 @@
 const Cryptr = require('./crypto-js.js');
 
 export async function onRequest(context) {
-    // Contents of context object
-    const {
-      request, // same as existing Worker API
-      env, // same as existing Worker API
-      params, // if filename includes [id] or [[path]]
-      waitUntil, // same as ctx.waitUntil in existing Worker API
-      next, // used for middleware or to fetch assets
-      data, // arbitrary space for passing data between middlewares
-    } = context;
-    const cryptr = new Cryptr('myTotallySecretKey');
+  // Contents of context object
+  const {
+    request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+    waitUntil, // same as ctx.waitUntil in existing Worker API
+    next, // used for middleware or to fetch assets
+    data, // arbitrary space for passing data between middlewares
+  } = context;
+  ciphertext = Cryptr.AES.encrypt('mess', 'myTotallySecretKey').toString();
+  return new Response("Hello, world!" + ciphertext);
+}
 
-    const encryptedString = cryptr.encrypt('bacon');
-    return new Response("Hello, world!");
-  }
+
